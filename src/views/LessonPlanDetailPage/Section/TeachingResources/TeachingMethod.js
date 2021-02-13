3cb6c91774ad02c70fb97ab7f60966ec766edcc9
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-02-13 13:11:01 -0600

    fix teaching method accordions

--- src/views/LessonPlanDetailPage/Section/TeachingResources/TeachingMethod.js
+++ src/views/LessonPlanDetailPage/Section/TeachingResources/TeachingMethod.js
@@ -1,6 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import ResourceSummary from "./ResourceSummary";
 import ResourceDownload from "./ResourceDownload/index";
@@ -9,28 +13,34 @@ import lessonPlanDetailsStyle from "assets/jss/material-kit-pro-react/views/less
 const useStyles = makeStyles(lessonPlanDetailsStyle);
 
 const TeachingMethod = ({
-  index,
   Title,
   ResourceSummary: Summary,
   ResourceDownloads = [],
+  expanded,
+  onChange,
 }) => {
   const classes = useStyles();
   return (
-    <div className="accordion TeachingMethod" defaultActiveKey={index}>
-      <div className={classes.container}>
-        <div className="accordion-toggle" variant="link" eventKey={index}>
-          <h3>{Title}</h3>
-        </div>
-      </div>
-      <div className="accordion-container" eventKey={index}>
+    <ExpansionPanel
+      className={"TeachingMethod"}
+      onChange={onChange}
+      expanded={expanded}
+    >
+      <ExpansionPanelSummary
+        className="ExpansionPanelSummary"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <h3>{Title}</h3>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
         <div className={classes.container}>
           <ResourceSummary {...Summary} />
           {ResourceDownloads.map((resource, i) => (
             <ResourceDownload key={i} {...resource} />
           ))}
         </div>
-      </div>
-    </div>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 };
 
@@ -39,6 +49,8 @@ TeachingMethod.propTypes = {
   Title: PropTypes.string,
   ResourceSummary: PropTypes.object,
   ResourceDownloads: PropTypes.array,
+  expanded: PropTypes.bool,
+  onChange: PropTypes.func,
 };
 
 export default TeachingMethod;
--- src/views/LessonPlanDetailPage/Section/TeachingResources/index.js
+++ src/views/LessonPlanDetailPage/Section/TeachingResources/index.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 
 import TeachingMethod from "./TeachingMethod";
 
 import "./style.scss";
 
-const TeachingResources = ({ TeachingMethod: TeachingMethods = [] }) =>
-  TeachingMethods.map((method, i) => (
-    <TeachingMethod
-      index={i + 1} // Accordion keys must be positive
-      key={i}
-      {...method}
-    />
-  ));
+import lessonPlanDetailsStyle from "assets/jss/material-kit-pro-react/views/lessonPlanDetailsStyle.js";
+const useStyles = makeStyles(lessonPlanDetailsStyle);
+
+const TeachingResources = ({ TeachingMethod: TeachingMethods = [] }) => {
+  const classes = useStyles();
+  const [expandedMethod, expandMethod] = useState(0);
+
+  const handleChange = (method) => {
+    expandMethod(method === expandedMethod ? null : method);
+  };
+
+  return (
+    <div className={classes.container}>
+      {TeachingMethods.map((method, i) => (
+        <TeachingMethod
+          expanded={expandedMethod === i}
+          onChange={() => handleChange(i)}
+          index={i + 1} // Accordion keys must be positive
+          key={i}
+          {...method}
+        />
+      ))}
+    </div>
+  );
+};
 
 TeachingResources.propTypes = {
   TeachingMethod: PropTypes.array,
