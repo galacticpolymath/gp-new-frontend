4571a9c1b83b2feb46b10f39a229a5bcec2dcd0d
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2021-02-22 17:29:00 -0600

    adjusted HireUs price list to look better on mobile and for rows to line up precisely on desktop.

--- src/assets/jss/material-kit-pro-react/views/hireUsSections/pricingStyle.js
+++ src/assets/jss/material-kit-pro-react/views/hireUsSections/pricingStyle.js
@@ -5,7 +5,6 @@ import {
   whiteColor,
   warningColor,
   hexToRgb, blackColor,
-
   primaryColor
 } from "assets/jss/material-kit-pro-react.js";
 
@@ -18,36 +17,36 @@ const pricingStyle = {
     color: whiteColor + " !important",
     "& small": {
       color: "rgba(" + hexToRgb(whiteColor) + ",0.6)!important",
-      paddingBottom: "2rem",
+      paddingBottom: "2rem"
     }
   },
   textCenter: {
-    textAlign: "center",
+    textAlign: "center"
   },
   textLeft: {
-    textAlign: "left !important",
+    textAlign: "left !important"
   },
   pricingSection: {
     padding: "80px 0px",
     // overflowX: "scroll",
-    scrollBehavior: "smooth",
+    scrollBehavior: "smooth"
   },
 
   textInfo: {
     color: primaryColor[0] + " !important"
   },
-  cardCategory:{
+  cardCategory: {
     fontWeight: 400
   },
 
   //new class style that makes an elevated, small $ before the number
-  dollar:{
-    "&:before":{
-      content: '"$"',
+  dollar: {
+    "&:before": {
+      content: "\"$\"",
       fontSize: "1rem",
       verticalAlign: "top",
       lineHeight: "2",
-      paddingRight: ".2rem",
+      paddingRight: ".2rem"
     }
 
   },
@@ -61,24 +60,22 @@ const pricingStyle = {
         ", 0.12), 0 8px 10px -5px rgba(" +
         hexToRgb(warningColor[0]) +
         ", 0.2)",
-      color: warningColor[0],
-    },
+      color: warningColor[0]
+    }
   },
-  valueProps:{
-    paddingTop:"3rem",
-    "& h2":{
-      textAlign: "center",
-    },
+  valueProps: {
+    paddingTop: "3rem",
+    "& h2": {
+      textAlign: "center"
+    }
   },
-  valuePropsGridContainer:{
-
-  },
-  appeal:{
-    padding: "5rem 10% 0 10%",
+  valuePropsGridContainer: {},
+  appeal: {
+    padding: "5rem 10% 0 10%"
 
   },
   boxContainer: {
-    backgroundColor:primaryColor[5],
+    backgroundColor: primaryColor[5],
     padding: "20px 20px 20px 40px",
     border: "3px solid" + primaryColor[7],
     borderRadius: "5px",
@@ -86,22 +83,33 @@ const pricingStyle = {
       // ...description,
 
       // fontWeight: "600",
-      color: primaryColor[0],
-    },
+      color: primaryColor[0]
+    }
   },
 
-  custInfoArea:{
-    padding:"0",
+  custInfoArea: {
+    padding: "0",
     // margin: "2rem 5rem",
-    maxWidth: "45%",
+    maxWidth: "45%"
   },
 
-  pfContainer:{
-    "&:hover":{
-      backgroundColor: "rgba("+hexToRgb(blackColor) +
-                        ", .1)",
-      fontSize: "1.1em",
-      },
+  pfContainer: {
+    // textAlign: "center",
+    // flexDirection: "row",
+
+    // gridTemplateRows: "1fr",
+    // gridTemplateAreas: "one two",
+    // flexDirection: "column",
+    // "& >*": {
+    //   textAlign: "left",
+    //   // flex: "1 1 content"
+    //
+    // },
+    "&:hover": {
+      backgroundColor: "rgba(" + hexToRgb(blackColor) +
+        ", .1)",
+      fontSize: "1.1em"
+    }
 
     // "& > *:hover":{
     //   // textShadow: "1px 1px 15px " +"rgba(" +
@@ -110,29 +118,32 @@ const pricingStyle = {
     //   fontSize: "1.1em"
     // }
   },
-  pfLink:{},
-  pfListItem:{
-  },
-  pfTXT:{
+  pfLink: {},
+  pfListItem: {},
 
-  },
-  boldedLabel:{
-
-  },
-  pfDescription:{
-
+  gridCont: {
+    display: "grid",
+    gridTemplateColumns: "content auto"
   },
 
+  pfTXT: {
+    "& > *": {
+      marginBottom: "-10px"
+    }
+  },
+  boldedLabel: {
 
-
+  },
+  pfDescription: {
+  },
 
   // keep infoAreas wider on smaller devices
-  "@media only screen and (max-width: 1000px)":{
-    custInfoArea:{
-      padding:"0",
+  "@media only screen and (max-width: 1000px)": {
+    custInfoArea: {
+      padding: "0",
       // margin: "2rem 5rem",
-      maxWidth: "80%",
-    },
+      maxWidth: "80%"
+    }
   }
 
 };
