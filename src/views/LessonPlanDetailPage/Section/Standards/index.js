6f867a60e73dd49428d2c8cbb76744946a23b2bc
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-02-24 21:51:18 -0600

    basic skeleton of standards

---
+++ src/views/LessonPlanDetailPage/Section/Standards/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import Subject from "./Subject";
+
+import lessonPlanStyle from "assets/jss/material-kit-pro-react/views/lessonPlanStyle.js";
+
+const useStyles = makeStyles(lessonPlanStyle);
+
+const Standards = ({ Data }) => {
+  const classes = useStyles();
+  const [expanded, expand] = useState(false);
+
+  return (
+    <div className={"Standards " + classes.container}>
+      <ExpansionPanel expanded={expanded} onChange={() => expand(!expanded)}>
+        <ExpansionPanelSummary
+          className="ExpansionPanelSummary"
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <h3>Standards and Assessments</h3>
+        </ExpansionPanelSummary>
+
+        <ExpansionPanelDetails>
+          <h3>Target Standard(s)</h3>
+          {Data.filter(({ target }) => target).map((subject, i) => (
+            <Subject key={"target-" + i} {...subject} />
+          ))}
+          <h3>Connected Standard(s)</h3>
+          {Data.filter(({ target }) => !target).map((subject, i) => (
+            <Subject key={"connected-" + i} {...subject} />
+          ))}
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+};
+
+Standards.propTypes = {
+  Data: PropTypes.array,
+};
+
+export default Standards;
