157506e71002709fa24bb2cfbf122f2f6fef6a68
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-03-08 19:59:43 -0600

    Revert "install implement react snap"
    This reverts commit 92fe769238e08ef3701bf6fa06cb9f649a986f46.

--- src/index.js
+++ src/index.js
@@ -15,13 +15,8 @@
 
 */
 import React from "react";
-import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(<App />, document.getElementById("root"));
