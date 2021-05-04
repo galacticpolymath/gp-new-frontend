c0de5b1ee29226d35d15e6be2505c36f7bd75538
Patrick Cheng <pcheng86@gmail.com>
2021-05-04 13:06:06 -0700

    better tracking

--- src/components/LessonDots/obs.js
+++ src/components/LessonDots/obs.js
@@ -28,15 +28,15 @@ const scrollDown = function(cursorBottom, elemOffsets, elemIds) {
     // you have the offset tops. find nearest one above current position by..
     // filtering out the ones below it and taking the last elem
     // filter:
-    const removeBelow = elemOffsets.filter(function (x) { return x < cursorBottom ; });
-    const index = removeBelow.length - 1
+    const removeBelow = elemOffsets.filter(function (x) { return x < cursorBottom - window.innerHeight/3 ; });
+    const index = removeBelow.length > 0 ? removeBelow.length - 1 : 0
 
     activateDot(index, elemIds)
 }
 
 const scrollUp = function(cursorTop, elemOffsets, elemIds) {
     // when you're scrolling up you want the nearest previous top.... find index - 1
-    const removeBelow = elemOffsets.filter(function (x) { return x < cursorTop ; });
+    const removeBelow = elemOffsets.filter(function (x) { return x < (cursorTop + window.innerHeight/3); });
     const index = removeBelow.length > 0 ? removeBelow.length - 1 : 0
 
     activateDot(index, elemIds)
@@ -50,7 +50,7 @@ export default function scrollHandler(){
 
     scrollElems.forEach(el => {
         elemIds.push(el.id.replace('&','\\&'))
-        elemOffsets.push(getOffset(el)) // get the offsetTops and put in dict
+        elemOffsets.push(getOffset(el)) 
     })
 
     const cursorTop = window.pageYOffset
@@ -64,27 +64,3 @@ export default function scrollHandler(){
     }
     lastOffset = window.pageYOffset
 }
-
-/*const scrollActions = function (){
-    
-
-    const offsets = Object.values(elemOffsets)
-    const removeMinuses = offsets.filter(function (x) { return x > -300; });
-    const minOffset = Math.min.apply(null, removeMinuses)
-    console.log(minOffset)
-    Object.keys(elemOffsets).forEach(elem=> {
-
-        const dotClass = elem.replace('&','\\&')
-        const dotNode = document.querySelector(`.${dotClass}`)
-        
-        if(elemOffsets[elem]==minOffset){
-            if(!dotNode.classList.contains('activeDot')){
-                dotNode.classList.add('activeDot')
-            }
-            else{
-                dotNode.classList.remove('activeDot')
-            }
-        }
-    })
-}
-*/
