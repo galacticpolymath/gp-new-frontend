9c74f38bf68f226bdb67d5ebe3ce14449c0b6382
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-05-05 18:55:07 -0500

    refresh dots on section update (#88)

--- src/components/LessonDots/DotPanel.js
+++ src/components/LessonDots/DotPanel.js
@@ -1,29 +1,33 @@
 import Dot from './Dot';
 import React, { useEffect, useState } from 'react';
 
-function Panel(props){
+const readHeadings = () => {
+    const titleArray = []
+    let headings = document.querySelectorAll('.SectionHeading')
+    headings.forEach((el)=>{
 
+        const indextitle = el.firstChild.textContent
+        const index = +indextitle.substr(0,indextitle.indexOf(' ')).slice(0,-1)
+        const title = indextitle.substr(indextitle.indexOf(' ')+1)
+        
+        titleArray.push([index, title, el.id])
+        //el.classList.add('scroll-indicator')
+    })
+    return titleArray
+}
+
+function Panel({
+    sections
+}){
     const [titles, setTitles] = useState([])
 
     // big assumption, all section headings start off with a number
     // have to do this because sectionheading structure is at uneven nesting levels so querySelectorAll brings them up out of order
     // also assuming the title content appears from the node's first child's content. appears to be consistent so far.
 
-    useEffect(()=>{
-        let titleArray = []
-
-        let headings = document.querySelectorAll('.SectionHeading')
-        headings.forEach((el)=>{
-
-            const indextitle = el.firstChild.textContent
-            const index = +indextitle.substr(0,indextitle.indexOf(' ')).slice(0,-1)
-            const title = indextitle.substr(indextitle.indexOf(' ')+1)
-            
-            titleArray.push([index, title, el.id])
-            //el.classList.add('scroll-indicator')
-        })
-        setTitles(titleArray)
-    },[])
+    useEffect(()=>{        
+        setTitles(readHeadings())
+    },[sections])
     
     let dots = titles.map((title)=>{
         return (<Dot section={title[2]} title={title[1]}/>)
