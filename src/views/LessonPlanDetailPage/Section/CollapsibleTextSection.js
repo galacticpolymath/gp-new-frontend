3f6d877dab0cd5341bdb11e9a66187289d9182ba
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-05-05 19:25:24 -0500

    collapsible text section

---
+++ src/views/LessonPlanDetailPage/Section/CollapsibleTextSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+
+import RichText from "../../../components/RichText";
+
+import lessonPlanStyle from "assets/jss/material-kit-pro-react/views/lessonPlanStyle.js";
+import CollapsibleSection from "./CollapsibleSection";
+const useStyles = makeStyles(lessonPlanStyle);
+
+const CollapsibleTextSection = ({ index, SectionTitle, Content }) => {
+  const classes = useStyles();
+  return (
+    <CollapsibleSection
+      className="CollapsibleTextSection"
+      index={index}
+      SectionTitle={SectionTitle}
+      initiallyExpanded
+    >
+      <div className={classes.container}>
+        <RichText content={Content} />
+      </div>
+    </CollapsibleSection>
+  );
+};
+
+CollapsibleTextSection.propTypes = {
+  Content: PropTypes.string,
+};
+
+export default CollapsibleTextSection;
