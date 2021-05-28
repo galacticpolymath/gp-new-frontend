8eddf5f460a21218cddab442fc50c9e7e970b847
Matt Wilkins <matt@galacticpolymath.com>
2021-05-27 22:48:58 -0500

    fixed OneZoom link and small copy tweak

--- src/views/AboutUsPage/Sections/GetInvolved.js
+++ src/views/AboutUsPage/Sections/GetInvolved.js
@@ -44,10 +44,9 @@ export default function GetInvolved() {
               }} />
           </h2>
           <h4>
-            We're a small, but dedicated team at the moment! If you're interested in
+            We're a small, but dedicated team at the moment. Please get in touch if you're interested in
             volunteering or contracting out your coding, webdev, art, graphic design, or lesson design skills;
-            collaborating in some other way; or perhaps joining the team full-time when funding is available–get in
-            touch!
+            collaborating in some other way; or perhaps joining the team full-time when funding is available!
           </h4>
           <h4>* If you're a grad student on a GRFP or an NSF-funded RA, please check out <a
             href={"https://docs.google.com/document/d/1fTYA8-2YQDPHyKN9SB23dLCcR77bbGOhwIG7SLN2yGg/preview"}>this
