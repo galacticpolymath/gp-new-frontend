520b7b0fb4118b10f6b72c9dd909c8830fdc7af8
Matt Wilkins <matt@galacticpolymath.com>
2021-05-31 12:26:42 -0500

    change variant notes icon in lesson plan step

--- src/views/LessonPlanDetailPage/Section/Procedure/Step.js
+++ src/views/LessonPlanDetailPage/Section/Procedure/Step.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
-import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import VariationsIcon from '@material-ui/icons/CallSplit';
 import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 
 import blogPostsPageStyle from "assets/jss/material-kit-pro-react/views/blogPostsPageStyle.js";
@@ -35,7 +35,7 @@ const LessonStep = ({
       )}
       {VariantNotes && (
         <div className="iconNote">
-          <QuestionAnswerIcon className="bulletIcon" />
+          <VariationsIcon className="bulletIcon" />
           <RichText content={VariantNotes} />
         </div>
       )}
