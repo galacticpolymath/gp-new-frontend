226210140bf344ec4739c27a80b444eb813a54c6
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-06-15 19:59:34 -0500

    update icons

--- src/views/LessonPlanDetailPage/icons.js
+++ src/views/LessonPlanDetailPage/icons.js
@@ -1,67 +1,84 @@
 import React from "react";
 
+/**
+ * Please alphabetize. Wrap all icons with
+ * <span role="img" aria-label="" />
+ * for accessibility.
+s */
 export default {
-  pdf: (
-    <span role="img" aria-label="pdf">
-      {/*📑*/}
-      <i className="far fa-file-pdf" style={{fontSize:"1.3rem"}}/>
+  computer: (
+    <span role="img" aria-label="computer">
+      <i className="fas fa-laptop" style={{ fontSize: "1.3rem" }} />
     </span>
-
   ),
   edit: (
     <span role="img" aria-label="edit">
-      {/*💾*/}
-      <i className="far fa-edit" style={{fontSize:"1.3rem"}}/>
+      <i className="far fa-edit" style={{ fontSize: "1.3rem" }} />
     </span>
   ),
-  present: (
-    <span role="img" aria-label="present">
-      {/*📽️*/}
-      <i className="far fa-play-circle" style={{fontSize:"1.1rem"}}/>
-    </span>
-  ),
-  nearpod: (
-    <span role="img" aria-label="nearpod">
-      🎧
-    </span>
-  ),
-  laptop: (
-    <span role="img" aria-label="laptop">
-      {/*💻*/}
-      <i className="fas fa-laptop" style={{fontSize:"1.3rem"}}/>
-    </span>
-
-  ),
-  projector: (
-    <span role="img" aria-label="projector">
-      📽️
-    </span>
-  ),
-  wifi: (
-    <span role="img" aria-label="wifi">
-      {/*📶*/}
-      <i className='fas fa-wifi'  style={{fontSize:"1.3rem"}}/>
+  inPerson: (
+    <span role="img" aria-label="internet">
+      <i className="fas fa-chalkboard-teacher" style={{ fontSize: "1.3rem" }} />
     </span>
   ),
   internet: (
     <span role="img" aria-label="internet">
-      {/*📶*/}
-      <i className='fas fa-wifi'  style={{fontSize:"1.3rem"}}/>
+      <i className="fas fa-wifi" style={{ fontSize: "1.3rem" }} />
     </span>
   ),
-  video: (
-    <span role="img" aria-label="video">
-      {/*📀*/}
-      <i className='fas fa-video'  style={{fontSize:"1.3rem"}}/>
+  laptop: (
+    <span role="img" aria-label="laptop">
+      <i className="fas fa-laptop" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  nearpod: (
+    <span role="img" aria-label="nearpod">
+      <i className="fas fa-headphones" style={{ fontSize: "1.3rem" }} />
     </span>
   ),
   pencil: (
     <span role="img" aria-label="pencil">
-      {/*📝*/}
-      <i className='far fa-edit'  style={{fontSize:"1.3rem"}}/>
+      <i className="far fa-edit" style={{ fontSize: "1.3rem" }} />
     </span>
   ),
-  inPerson: (
-  <i className='fas fa-chalkboard-teacher'  style={{fontSize:"1.3rem"}}/>
-)
+  pdf: (
+    <span role="img" aria-label="pdf">
+      <i className="far fa-file-pdf" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  present: (
+    <span role="img" aria-label="present">
+      <i className="far fa-play-circle" style={{ fontSize: "1.1rem" }} />
+    </span>
+  ),
+  projector: (
+    <span role="img" aria-label="projector">
+      <i className="fas fa-video" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  speaker: (
+    <span role="img" aria-label="speakers">
+      <i className="fas fa-volume-up" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  table: (
+    <span role="img" aria-label="table">
+      <i className="fas fa-table" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  video: (
+    <span role="img" aria-label="video">
+      <i className="fas fa-video" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  wifi: (
+    <span role="img" aria-label="wifi">
+      <i className="fas fa-wifi" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
+  worksheet: (
+    <span role="img" aria-label="worksheet">
+      <i className="far fa-file-alt" style={{ fontSize: "1.3rem" }} />
+    </span>
+  ),
 };
