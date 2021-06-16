536791152cae307768fc34ff1e16e790f1ecbf35
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-06-15 20:34:07 -0500

    copy and icons for teaching methods

--- src/views/LessonPlanDetailPage/Section/TeachingResources/TeachingMethod2.js
+++ src/views/LessonPlanDetailPage/Section/TeachingResources/TeachingMethod2.js
@@ -4,11 +4,40 @@ import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import SchoolIcon from "@material-ui/icons/School";
+import RemoteIcon from "@material-ui/icons/Laptop";
 
 import ResourceSummary2 from "./ResourceSummary2";
 
+const COPY = {
+  remote: (
+    <p>
+      Everything you need to teach the lesson in person. Classroom lessons are
+      designed to be led by a teacher, who will actively moderate discussions.
+      Students will record their observations and reflections on printed
+      handouts, as opposed to the remote version, where students respond
+      digitally to embedded questions using Nearpod on their laptop.
+    </p>
+  ),
+  in_person: (
+    <p>
+      Everything you need to teach the lesson in person. Classroom lessons are
+      designed to be led by a teacher, who will actively moderate discussions.
+      Students will record their observations and reflections on printed
+      handouts, as opposed to the remote version, where students respond
+      digitally to embedded questions using Nearpod on their laptop.
+    </p>
+  ),
+};
+const ICONS = {
+  remote: <RemoteIcon />,
+  in_person: <SchoolIcon />,
+};
+
 const TeachingMethod2 = ({ title, resourceSummary }) => {
   const [expanded, expand] = useState(false);
+  const mode = title.toLowerCase().includes("remote") ? "remote" : "in_person";
+
   return (
     <ExpansionPanel
       className="TeachingMethod ExpansionPanel"
@@ -19,7 +48,12 @@ const TeachingMethod2 = ({ title, resourceSummary }) => {
         className="ExpansionPanelSummary"
         expandIcon={<ExpandMoreIcon />}
       >
-        <h3>{title}</h3>
+        <div>
+          <h3>
+            {ICONS[mode]} {title}
+          </h3>
+          {COPY[mode]}
+        </div>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails className="ExpansionPanelDetails">
         <div>
