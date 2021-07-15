7864e0e0c7f3ebe8a7758fc72400359a7a4c9f56
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-07-15 17:33:39 -0500

    only import and export in pages/index

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,16 +1,10 @@
-import React from "react";
-// nodejs library to set properties for components
-// nodejs library that concatenates classes
+import React, { useEffect } from "react";
 import classNames from "classnames";
-// @material-ui/core components
+
 import { makeStyles } from "@material-ui/core/styles";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// @material-ui/icons
 import BlurOn from "@material-ui/icons/BlurOn";
 import CreateIcon from "@material-ui/icons/Create";
 
-// core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -21,25 +15,25 @@ import Parallax from "components/Parallax/Parallax.js";
 
 import landingPageStyle from "assets/jss/material-kit-pro-react/views/landingPageStyle.js";
 
-// Sections for this page
-// import SectionProduct from "./Sections/SectionProduct.js";
-// import SectionTeam from "./Sections/SectionTeam.js";
-// import SectionWork from "./Sections/SectionWork.js";
-import SectionAbout from "./Sections/SectionAbout.js";
-import SectionLessons from "./Sections/SectionLessons.js";
-import SectionHow from "./Sections/SectionHow.js";
-import SectionLessonData from "./Sections/SectionLessonData.js";
-import SectionSubscribe from "./Sections/SectionSubscribe.js";
+import SectionAbout from "views/LandingPage/Sections/SectionAbout.js";
+import SectionLessons from "views/LandingPage/Sections/SectionLessons.js";
+import SectionHow from "views/LandingPage/Sections/SectionHow.js";
+import SectionLessonData from "views/LandingPage/Sections/SectionLessonData.js";
+import SectionSubscribe from "views/LandingPage/Sections/SectionSubscribe.js";
 
-import GPcopyrightFooter from "../../components/Footer/GPcopyrightFooter";
+import GPcopyrightFooter from "components/Footer/GPcopyrightFooter";
+
+import ParallaxImage from "assets/img/hero-images/landingPage-CityNetwork@75x-20.jpg";
+
+import "assets/scss/material-kit-pro-react.scss?v=1.9.0";
 
 const useStyles = makeStyles(landingPageStyle);
 
-export default function LandingPage({ ...rest }) {
-  React.useEffect(() => {
+export default function LandingPage(props) {
+  useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
-  });
+  }, []);
   const classes = useStyles();
   return (
     <div style={{ backgroundColor: "#470F2C" }}>
@@ -52,11 +46,9 @@ export default function LandingPage({ ...rest }) {
           height: 50,
           color: "dark",
         }}
-        {...rest}
+        {...props}
       />
-      <Parallax
-        image={require("assets/img/hero-images/landingPage-CityNetwork@75x-20.jpg")}
-      >
+      <Parallax image={ParallaxImage}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={6} md={6}>
@@ -101,54 +93,12 @@ export default function LandingPage({ ...rest }) {
         </div>
         <SectionLessonData />
 
-          <SectionSubscribe />
-          <br/>
-          {/* <SectionProduct />
-          <SectionTeam />
-          <SectionWork /> */}
-
+        <SectionSubscribe />
       </div>
       <Footer
         theme={"transparent"}
         content={
           <div>
-            {/*<div className={classes.left}>*/}
-            {/*  <List className={classes.list}>*/}
-            {/*    <ListItem className={classes.inlineBlock}>*/}
-            {/*      <a*/}
-            {/*        href="https://www.creative-tim.com/?ref=mkpr-landing"*/}
-            {/*        target="_blank"*/}
-            {/*        className={classes.block}*/}
-            {/*      >*/}
-            {/*        Creative Tim*/}
-            {/*      </a>*/}
-            {/*    </ListItem>*/}
-            {/*    <ListItem className={classes.inlineBlock}>*/}
-            {/*      <a*/}
-            {/*        href="https://www.creative-tim.com/presentation?ref=mkpr-landing"*/}
-            {/*        target="_blank"*/}
-            {/*        className={classes.block}*/}
-            {/*      >*/}
-            {/*        About us*/}
-            {/*      </a>*/}
-            {/*    </ListItem>*/}
-            {/*    <ListItem className={classes.inlineBlock}>*/}
-            {/*      <a href="//blog.creative-tim.com/" className={classes.block}>*/}
-            {/*        Blog*/}
-            {/*      </a>*/}
-            {/*    </ListItem>*/}
-            {/*    <ListItem className={classes.inlineBlock}>*/}
-            {/*      <a*/}
-            {/*        href="https://www.creative-tim.com/license?ref=mkpr-landing"*/}
-            {/*        target="_blank"*/}
-            {/*        className={classes.block}*/}
-            {/*      >*/}
-            {/*        Licenses*/}
-            {/*      </a>*/}
-            {/*    </ListItem>*/}
-            {/*  </List>*/}
-            {/*</div>*/}
-
             <div style={{ color: "white", opacity: ".7" }}>
               <GPcopyrightFooter />
             </div>
