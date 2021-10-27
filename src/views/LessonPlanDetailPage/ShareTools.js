9457295365d752021b9ff58fc72476c6bdaebfe4
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-10-27 17:04:25 -0500

    add twitter

--- src/views/LessonPlanDetailPage/ShareTools.js
+++ src/views/LessonPlanDetailPage/ShareTools.js
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react';
 
 import Facebook from 'assets/img/share-logos/fb.svg'
 import Pinterest from 'assets/img/share-logos/pinterest.svg'
+import Twitter from 'assets/img/share-logos/twitter.svg'
 
 const networks = [
   {
-    name: "Facebook",
+    name: 'Facebook',
     link: 'https://www.facebook.com/sharer/sharer.php?u=',
     image: Facebook
   },
   {
-    name: "Pinterest",
+    name: 'Twitter',
+    link: 'https://twitter.com/intent/tweet?text=',
+    image: Twitter
+  },
+  {
+    name: 'Pinterest',
     link: 'https://pinterest.com/pin/create/link/?url=',
     image: Pinterest
   }
@@ -22,13 +28,13 @@ const ShareTools = ({ location }) => {
     setShareUrl(encodeURI(window.location.origin + location.pathname))
   }, [])
 
-  return <div id="ShareTools">
+  return <div id='ShareTools'>
     <label>Share:</label>
     {networks.map(({ name, link, image }) =>
       <a
         key={name}
-        rel="noopener noreferrer"
-        target="_blank"
+        rel='noopener noreferrer'
+        target='_blank'
         href={`${link}${shareUrl}`}
       >
         <img src={image} alt={name} />
