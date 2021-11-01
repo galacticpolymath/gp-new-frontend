df5d8a0e7bb62a5030f9c0da880a1946612d9437
McKenzie Bottoms <mbottoms@razorsync.com>
2021-11-01 09:44:20 -0500

    remove commented out code, fix eslint issues in contact us page

--- src/views/ContactUsPage/ContactUsPage.js
+++ src/views/ContactUsPage/ContactUsPage.js
@@ -1,14 +1,6 @@
-/*eslint-disable*/
 import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
-// react components used to create a google map
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
@@ -30,81 +22,6 @@ import Footer from "components/Footer/Footer.js";
 import Parallax from "components/Parallax/Parallax.js";
 
 import contactUsStyle from "assets/jss/material-kit-pro-react/views/contactUsStyle.js";
-
-// const CustomSkinMap = withScriptjs(
-//   withGoogleMap(() => (
-//     <GoogleMap
-//       defaultZoom={14}
-//       defaultCenter={{ lat: 44.43353, lng: 26.093928 }}
-//       defaultOptions={{
-//         scrollwheel: false,
-//         zoomControl: true,
-//         styles: [
-//           {
-//             featureType: "water",
-//             stylers: [
-//               { saturation: 43 },
-//               { lightness: -11 },
-//               { hue: "#0088ff" },
-//             ],
-//           },
-//           {
-//             featureType: "road",
-//             elementType: "geometry.fill",
-//             stylers: [
-//               { hue: "#ff0000" },
-//               { saturation: -100 },
-//               { lightness: 99 },
-//             ],
-//           },
-//           {
-//             featureType: "road",
-//             elementType: "geometry.stroke",
-//             stylers: [{ color: "#808080" }, { lightness: 54 }],
-//           },
-//           {
-//             featureType: "landscape.man_made",
-//             elementType: "geometry.fill",
-//             stylers: [{ color: "#ece2d9" }],
-//           },
-//           {
-//             featureType: "poi.park",
-//             elementType: "geometry.fill",
-//             stylers: [{ color: "#ccdca1" }],
-//           },
-//           {
-//             featureType: "road",
-//             elementType: "labels.text.fill",
-//             stylers: [{ color: "#767676" }],
-//           },
-//           {
-//             featureType: "road",
-//             elementType: "labels.text.stroke",
-//             stylers: [{ color: "#ffffff" }],
-//           },
-//           { featureType: "poi", stylers: [{ visibility: "off" }] },
-//           {
-//             featureType: "landscape.natural",
-//             elementType: "geometry.fill",
-//             stylers: [{ visibility: "on" }, { color: "#b8cb93" }],
-//           },
-//           { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-//           {
-//             featureType: "poi.sports_complex",
-//             stylers: [{ visibility: "on" }],
-//           },
-//           { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-//           {
-//             featureType: "poi.business",
-//             stylers: [{ visibility: "simplified" }],
-//           },
-//         ],
-//       }}
-//     >
-//       <Marker position={{ lat: 44.43353, lng: 26.093928 }} />
-//     </GoogleMap>
-//   ))
-// );
 
 const useStyles = makeStyles(contactUsStyle);
 
@@ -170,22 +87,6 @@ export default function ContactUsPage() {
           </GridContainer>
         </div>
       </Parallax>
-      {/* <div className={classes.bigMap}>
-        <CustomSkinMap
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={
-            <div
-              style={{
-                height: `100%`,
-                borderRadius: "6px",
-                overflow: "hidden",
-              }}
-            />
-          }
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div> */}
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.contactContent}>
           <div className={classes.container}>
@@ -288,7 +189,8 @@ export default function ContactUsPage() {
                 <ListItem className={classes.inlineBlock}>
                   <a
                     href="https://www.creative-tim.com/?ref=mkpr-contact-us"
-                    target="_blank"
+                    target='_blank'
+                    rel='noopener noreferrer'
                     className={classes.block}
                   >
                     Creative Tim
@@ -297,21 +199,28 @@ export default function ContactUsPage() {
                 <ListItem className={classes.inlineBlock}>
                   <a
                     href="https://www.creative-tim.com/presentation?ref=mkpr-contact-us"
-                    target="_blank"
+                    target='_blank'
+                    rel='noopener noreferrer'
                     className={classes.block}
                   >
                     About us
                   </a>
                 </ListItem>
                 <ListItem className={classes.inlineBlock}>
-                  <a href="//blog.creative-tim.com/" className={classes.block}>
+                  <a 
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href="//blog.creative-tim.com/"
+                    className={classes.block}
+                  >
                     Blog
                   </a>
                 </ListItem>
                 <ListItem className={classes.inlineBlock}>
                   <a
                     href="https://www.creative-tim.com/license?ref=mkpr-contact-us"
-                    target="_blank"
+                    target='_blank'
+                    rel='noopener noreferrer'
                     className={classes.block}
                   >
                     Licenses
@@ -324,7 +233,8 @@ export default function ContactUsPage() {
               <Favorite className={classes.icon} /> by{" "}
               <a
                 href="https://www.creative-tim.com?ref=mkpr-contact-us"
-                target="_blank"
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Creative Tim
               </a>{" "}
