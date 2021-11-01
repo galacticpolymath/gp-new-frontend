f0139eafeaa76eca9516c29ddfc46ac4fa5e28b2
McKenzie Bottoms <mbottoms@razorsync.com>
2021-11-01 10:21:58 -0500

    default meta image to logo

--- src/utils/meta.js
+++ src/utils/meta.js
@@ -3,11 +3,11 @@ import { Helmet } from 'react-helmet'
 
 /**
  * Render a Helmet with meta tags.
- * @param {string} title - Meta title text
+ * @param {string} [title] - Meta title text
  * @param {string} description - Meta description text
- * @param {string} image - Meta image URL
+ * @param {string} [image] - Meta image URL
  */
-export const renderMetaTags = (title = '', description, image) => {
+export const renderMetaTags = (title = '', description, image = 'https://res.cloudinary.com/galactic-polymath/image/upload/v1593304395/logos/GP_full_stacked_grad_whiteBG_llfyal.png') => {
   const fullTitle = title === '' ? 'Galactic Polymath' : 'Galactic Polymath | '+title
   return <Helmet>
     <title>{fullTitle}</title>
