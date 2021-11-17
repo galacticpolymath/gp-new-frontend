22d6f0ce8cced05cfce77ae5e2fdef3e1c55e003
McKenzie Bottoms <mbottoms@razorsync.com>
2021-11-17 13:33:58 -0600

    add og:type article

--- src/utils/meta.js
+++ src/utils/meta.js
@@ -11,18 +11,19 @@ export const renderMetaTags = ({
   title = '',
   description,
   image = 'https://res.cloudinary.com/galactic-polymath/image/upload/v1593304395/logos/GP_full_stacked_grad_whiteBG_llfyal.png',
-  url = 'https://www.galacticpolymath.com'
+  url = 'https://www.galacticpolymath.com',
+  type = 'article',
 }) => {
   const fullTitle = title === '' ? 'Galactic Polymath' : 'Galactic Polymath | '+title
   return <Helmet>
     <title>{fullTitle}</title>
-    <meta property="og:title" content={fullTitle} />
+    <meta name="og:title" property="og:title" content={fullTitle} />
 
-    <meta property="og:description" content={description} />
-    <meta name="description" content={description} />
+    <meta name="og:description" property="og:description" content={description} />
+    <meta name="description" property="description" content={description} />
 
-    <meta name="og:image" content={image} />
-
-    <meta name="og:url" content={url} />
+    <meta name="og:image" property="og:image" content={image} />
+    <meta name="og:url" property="og:url" content={url} />
+    <meta name="og:type" property="og:type" content={type} />
   </Helmet>
 }
