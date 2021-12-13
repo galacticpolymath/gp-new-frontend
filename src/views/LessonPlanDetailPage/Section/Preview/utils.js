190c34f2bd515a83b91f60fca29228796f586f9b
McKenzie Bottoms <mbottoms@razorsync.com>
2021-12-13 13:21:29 -0600

    secure youtube thumb

--- src/views/LessonPlanDetailPage/Section/Preview/utils.js
+++ src/views/LessonPlanDetailPage/Section/Preview/utils.js
@@ -8,7 +8,7 @@ const getVideoThumb = link => {
 
   if (link.includes('youtube')) {
     const slug = link.split('/embed/')[1]
-    return `http://i3.ytimg.com/vi/${slug}/hqdefault.jpg`
+    return `https://i3.ytimg.com/vi/${slug}/hqdefault.jpg`
   }
   return ''
 }
