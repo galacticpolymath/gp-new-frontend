6244d365b6afd4917d647ee69e15411d213ebb19
McKenzie Bottoms <mbottoms@razorsync.com>
2022-01-19 17:25:18 -0600

    hijack links to make sure internals are smooth and externals use new tab

--- src/components/CustomMarkdownView/index.js
+++ src/components/CustomMarkdownView/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import MarkdownView from "react-showdown";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import ExternalLink from 'components/ExternalLink';
 
 const components = {
   AnchorLink (props) {
     return <AnchorLink offset="125px" {...props} />
+  },
+  a (props) {
+    let Link = 'a'
+
+    // if the link DOES NOT go to galacticpolymath.com/* (excluding subdomains)
+    if (!/^(https?:)?(\/\/)?(www\.)?galacticpolymath\.com/.test(props.href)) {
+      Link = ExternalLink
+    }
+
+    return <Link {...props} />
   }
 }
 
