8638b8758d0db3f51e735e03b4a987aa104215ae
Kenzie Bottoms <kenzie@everydaylabs.com>
2022-03-31 20:11:18 -0500

    handle lesson without version section

--- src/views/LessonPlanDetailPage/Header.js
+++ src/views/LessonPlanDetailPage/Header.js
@@ -16,6 +16,8 @@ const useStyles = makeStyles(lessonPlanStyle);
 
 const getLatestSubRelease = (sections) => {
   const versionSection = sections.versions
+  if (!versionSection) return null;
+  
   const lastRelease =
     versionSection.Data[versionSection.Data.length - 1].sub_releases;
   const lastSubRelease = lastRelease[lastRelease.length - 1];
