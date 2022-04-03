726d7f56ea6069ffc434ab79c3dec2fc2fb02257
Matt Wilkins <matt@galacticpolymath.com>
2022-04-03 14:43:45 -0500

    simplify Remote and In-person descriptions

--- src/views/LessonPlanDetailPage/Section/TeachingResources/constants.js
+++ src/views/LessonPlanDetailPage/Section/TeachingResources/constants.js
@@ -16,21 +16,13 @@ export const TITLES = {
 export const COPY = {
   [METHODS.REMOTE]: (
     <p>
-      All you need to teach the lesson remotely, using the Nearpod platform.
-      This version can be taught in a teacher-paced format (over a live video
-      call) or a student-paced format (where students work independently).
-      Either version can also be used in an in-person classroom setting, and the
-      student-paced version is great for a substitute teacher to run a rigorous
-      lesson you had intended to teach in person.
+      Designed for remote teaching, using the Nearpod platform.
+      It can also be used in-person, and the student-paced version is great for a sub plan.
     </p>
   ),
   [METHODS.IN_PERSON]: (
     <p>
-      Everything you need to teach the lesson in person. Classroom lessons are
-      designed to be led by a teacher, who will actively moderate discussions.
-      Students will record their observations and reflections on printed
-      handouts, as opposed to the remote version, where students respond
-      digitally to embedded questions using Nearpod on their laptop.
+      Designed to be led by a non-specialist teacher, in person.
     </p>
   ),
 };
