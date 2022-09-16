6070a574c47c118e2546a5e7bf7abdb147a37962
Matt Wilkins <matt@galacticpolymath.com>
2022-09-16 13:10:29 -0500

    added subscribe appeal below teaching Mat

--- src/views/LessonPlanDetailPage/Section/TeachingResources/index.js
+++ src/views/LessonPlanDetailPage/Section/TeachingResources/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
+// @mui material icons
+import MailIcon from "@material-ui/icons/Mail";
+import { RateReviewOutlined } from "@material-ui/icons";
+
 import LessonHelperText from "components/LessonHelperText";
 import TeachingMethod from "./TeachingMethod";
 import CollapsibleSection from "../CollapsibleSection";
@@ -10,6 +14,9 @@ import { METHODS } from "./constants";
 
 import "./style.scss";
 import lessonPlanStyle from "assets/jss/material-kit-pro-react/views/lessonPlanStyle.js";
+import GridContainer from "components/Grid/GridContainer";
+import GridItem from "components/Grid/GridItem";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const useStyles = makeStyles(lessonPlanStyle);
 
@@ -29,7 +36,7 @@ const TeachingResources = ({
       SectionTitle={SectionTitle}
     >
       <div className={classes.container}>
-        {classroom && remote && <LessonHelperText text="Click a category for more details"/>}
+        {classroom && remote && <LessonHelperText text="Click a category for more details" />}
         {classroom && (
           <TeachingMethod
             type={METHODS.IN_PERSON}
@@ -46,6 +53,22 @@ const TeachingResources = ({
             {...remote}
           />
         )}
+        <GridContainer style={{border: "1pt solid #363636",marginBottom: "1.5rem"}}>
+          <GridItem>
+          <h5>Please help us make more lessons like this!</h5>
+            <a href="https://bit.ly/gpEmails" target="_blank" rel="noopener noreferrer">
+              <MailIcon style={{marginBottom:"-0.35rem",marginRight:"0.25rem"}}/> 
+              Subscribe to emails
+            </a>
+            <div style={{marginBottom:"1rem"}}>
+            <AnchorLink href="#feedback" offset="125px">
+             <RateReviewOutlined style={{marginBottom:"-0.35rem",marginRight:"0.25rem"}}/> 
+             Give feedback to help us measure impact and improve
+            </AnchorLink>
+            </div>
+          </GridItem>
+        </GridContainer>
+      
       </div>
     </CollapsibleSection>
   );
